a7bbefd5e8c8a7b715fcf91bf741133875113b92
Your Name <tajempey@gmail.com>
2023-11-17 09:47:54 -0700

    WIP

---
+++ src/calculate.js
@@ -0,0 +1,16 @@
+export default class ExchangeRate {  
+  static getRate(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })      
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
